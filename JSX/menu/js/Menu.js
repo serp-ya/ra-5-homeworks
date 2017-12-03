440195a1d7e040d57da76801044faf175c699da2
serp-ya <sergei.kundryukov1989@gmail.com>
2017-12-03 20:26:25 +0300

    Add files via upload

--- JSX/menu/js/Menu.js
+++ JSX/menu/js/Menu.js
@@ -2,30 +2,24 @@ function Menu(data) {
   const isOpened = data.opened;
   const itemsData = data.items;
 
-  if (!isOpened) {
-    return (
-      <div className="menu">
-        <div className="menu-toggle"><span></span></div>
-      </div>
-    );
-  }
-
   const menuItems = itemsData.map((item, index) => {
     return (
-      <li key={item.index}>
+      <li key={index}>
         <a href={item.href}>{item.title}</a>
       </li>
     );
   });
 
   return (
-    <div className="menu menu-open">
+    <div className={(isOpened ? "menu menu-open" : "menu")}>
       <div className="menu-toggle"><span></span></div>
-      <nav>
-        <ul>
-          {menuItems}
-        </ul>
-      </nav>
+      {isOpened && (
+        <nav>
+          <ul>
+            {menuItems}
+          </ul>
+        </nav>
+      )}
     </div>
   );
 }
