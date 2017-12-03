ec3f40806b800b77db9959629f471bdaa1f1480e
serp-ya <sergei.kundryukov1989@gmail.com>
2017-12-03 20:27:28 +0300

    Add files via upload

--- JSX/store/js/ShopItem.js
+++ JSX/store/js/ShopItem.js
@@ -3,9 +3,9 @@ function ShopItem(data) {
 
   return (
     <div className="main-content">
-      <h2> {brand} </h2>
-      <h1> {title} </h1>
-      <h3> {description} </h3>
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
 
       <div className="description">
         {descriptionFull}
